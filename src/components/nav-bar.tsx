7fa1dbe427901957b60a8975da81e94f3f97233e
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-09-18 01:28:28 +0100

    feat: created nav bar component

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,20 @@
+/**@jsx jsx */
+import { jsx, css } from '@emotion/core'
+import React from 'react'
+
+const navStyle = css({
+  backgroundColor: '#24292E',
+  padding: '12px',
+})
+
+export const Navbar: React.FC = () => (
+  <nav css={navStyle}>
+    <div>
+      <img
+        src="../logo.png"
+        alt="github logo"
+        style={{ paddingLeft: '10px' }}
+      />
+    </div>
+  </nav>
+)
